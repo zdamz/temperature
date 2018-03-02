41a4ddadbebae9026eeff711ac056de6bb9bfc6d
zdamz <damien.labelle@gmx.fr>
2018-03-02 10:48:18 +0100

    latittude logitude

--- js/app.js
+++ js/app.js
@@ -14,9 +14,10 @@ $(document).ready(function(){
         type: "GET",
         dataType: "jsonp",
         success: function(data) {
-          $("#temp").html( parseInt(data.main.temp) + "°")
-          $("#reste").html("temp.max:"+ data.main.temp_max+"°"+ "<br/> temp.min:"+ data.main.temp_min+"°"+"<br/> Pression:" + data.main.pressure+"hPa" + "<br/> Vitesse du vent: " + data.wind.speed + "km/h" + "<br/> Humidité: "+ data.main.humidity +"%")
+          $("#temp").html( parseInt(data.main.temp) + "°");
+          $("#reste").html("temp.max:"+ data.main.temp_max+"°"+ "<br/> temp.min:"+ data.main.temp_min+"°"+"<br/> Pression:" + data.main.pressure+"hPa" + "<br/> Vitesse du vent: " + data.wind.speed + "km/h" + "<br/> Humidité: "+ data.main.humidity +"%");
           $("#mapid").html("<iframe height='450' width='100%' frameborder='0' src='https://www.google.com/maps/embed/v1/place?key=AIzaSyBSFz0My4qAmKZTgMGd5hYGy8egXCuT8xI&q="+ city +"&zoom=12&maptype=satellite'></iframe>");// console.log(data)
+          $("#latLong").html("latittude:" + data.coord.lat + " longitude:" + data.coord.lon);
         }
         // console.log($("#mapid").html("<iframe>width='600' height='450'frameborder='0' style='border:0' src='https://www.google.com/maps/embed/v1/place?key=AIzaSyDSnzCZZF-O3rpnlvJEJeJgsVbph2w8kjQ&q="+ city + "allowfullscreen></iframe>"));
 
